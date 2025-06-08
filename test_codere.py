ea0639348bcec36fa36c5ec0ce2ae7a31218b202
vladi014 <vladandrei1401@outlook.es>
2025-06-08 19:51:16 +0200

    Update shebangs for Python 3

--- test_codere.py
+++ test_codere.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python
+#!/usr/bin/env python3
 # coding: latin-1
 
 # Colas que necesitaremos para los hilos
@@ -24,58 +24,58 @@ def similar(a, b):
 
 # Realizar esperas entre solicitudes
 if __name__ == "__main__":
-	cola1 = Queue.Queue()
-	cola2 = Queue.Queue()
-	cola3 = Queue.Queue()
-	cola4 = Queue.Queue()
-	cola5 = Queue.Queue()
-	cola6 = Queue.Queue()
+        cola1 = queue.Queue()
+        cola2 = queue.Queue()
+        cola3 = queue.Queue()
+        cola4 = queue.Queue()
+        cola5 = queue.Queue()
+        cola6 = queue.Queue()
 
-	maximo = raw_input("INTRODUCE EL DINERO MAXIMO A APOSTAR: ")
+        maximo = float(input("INTRODUCE EL DINERO MAXIMO A APOSTAR: "))
 
-	codere = ThreadCodere(cola1, cola2, cola5, cola6)
-	bet = ThreadBet(cola1, cola2, cola3, cola4)
-	
-	codere.start()
-	bet.start()
-	# Siempre bet es el primero en comprobar para obtener valor redondo
-	# Cola 4 es bet y cola 6 es codere
-	while True:
-		datos_bet = cola3.get()
-		datos_codere = cola5.get()
-		cotizaciones = comprobar_cotizaciones(datos_bet[0], datos_codere[1], maximo)
-		if (cotizaciones[0] == True):
-			try:
-				a = cotizaciones[1]
-				b = cotizaciones[2]
-				cola4.put([True, a, 0])
-				cola6.put([True, 0, b])
-				print("SURE BET ENCONTRADA")
-				fichero = open("SURE_BET/sure_completo.txt", "a")
-				fichero.write(str(a + b) + " " + str(maximo)+ "\n")
-				fichero.close()
-			except IndexError as e:
-				cola4.put([False])
-				cola6.put([False])
-		else:
-			cotizaciones = comprobar_cotizaciones(datos_bet[1], datos_codere[0], 10)
-			if (cotizaciones[0] == True):
-				try:
-					a = cotizaciones[1]
-					b = cotizaciones[2]
-					cola4.put([True, 0, a])
-					cola6.put([True, b, 0])
-					print("SURE BET ENCONTRADA")
-					fichero = open("SURE_BET/sure_completo.txt", "a")
-					fichero.write(str(a + b) + " " + str(maximo)+ "\n")
-					fichero.close()
-				except IndexError as e:
-					cola4.put([False])
-					cola6.put([False])
-			else:
-				cola4.put([False])
-				cola6.put([False])
+        codere = ThreadCodere(cola1, cola2, cola5, cola6)
+        bet = ThreadBet(cola1, cola2, cola3, cola4)
+        
+        codere.start()
+        bet.start()
+        # Siempre bet es el primero en comprobar para obtener valor redondo
+        # Cola 4 es bet y cola 6 es codere
+        while True:
+                datos_bet = cola3.get()
+                datos_codere = cola5.get()
+                cotizaciones = comprobar_cotizaciones(datos_bet[0], datos_codere[1], maximo)
+                if (cotizaciones[0] == True):
+                        try:
+                                a = cotizaciones[1]
+                                b = cotizaciones[2]
+                                cola4.put([True, a, 0])
+                                cola6.put([True, 0, b])
+                                print("SURE BET ENCONTRADA")
+                                fichero = open("SURE_BET/sure_completo.txt", "a")
+                                fichero.write(str(a + b) + " " + str(maximo)+ "\n")
+                                fichero.close()
+                        except IndexError as e:
+                                cola4.put([False])
+                                cola6.put([False])
+                else:
+                        cotizaciones = comprobar_cotizaciones(datos_bet[1], datos_codere[0], 10)
+                        if (cotizaciones[0] == True):
+                                try:
+                                        a = cotizaciones[1]
+                                        b = cotizaciones[2]
+                                        cola4.put([True, 0, a])
+                                        cola6.put([True, b, 0])
+                                        print("SURE BET ENCONTRADA")
+                                        fichero = open("SURE_BET/sure_completo.txt", "a")
+                                        fichero.write(str(a + b) + " " + str(maximo)+ "\n")
+                                        fichero.close()
+                                except IndexError as e:
+                                        cola4.put([False])
+                                        cola6.put([False])
+                        else:
+                                cola4.put([False])
+                                cola6.put([False])
 
-	fichero.close()
-	codere.parar()
-	bet.parar()
+        fichero.close()
+        codere.parar()
+        bet.parar()
